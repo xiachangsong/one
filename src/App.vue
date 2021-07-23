175c2e5b944547720c3644dbfedd84e2b6973ed4
xiachangsong <1065256104@qq.com>
2021-07-23 14:31:04 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="main">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* 全局初始化 */
+  *{ margin: 0;padding: 10px;list-style: none;outline: none;text-decoration: none;}
+  .flex{display: flex;}
+  .j-c{justify-content: center;}
+  .j-s{justify-content: space-between;}
+  .a-c{align-items: center;}
+</style>
+
+<style scoped>
+  .main{
+    border: 1px solid black;
+  }
+</style>>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //动了配置文件就需要重启项目
+  lintOnSave: false,//禁用esint
+}
